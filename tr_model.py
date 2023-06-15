f16b20abe56ab486069b1a9e18ce0255fd1c3e7c
ilyagladky1981 <ilyagladky1981@gmail.com>
2023-06-15 12:24:20 +0000

    Model training part 2.

--- tr_model.py
+++ tr_model.py
@@ -17,8 +17,8 @@ def train_model_by_img(name):
   for(i, image) in enumerate(images):
     print(f"[+] processing img {i + 1}/{len(images)}")
     image = image.strip()
-    print(f"image={image}")
-    print(f"dataset/{image}")
+    # print(f"image={image}")
+    # print(f"dataset/{image}")
     
     face_img = face_recognition.load_image_file(f"dataset/{image}")
     face_enc = face_recognition.face_encodings(face_img)[0]
@@ -30,19 +30,28 @@ def train_model_by_img(name):
     else:
       for item in range(0, len(known_encodings)):
         result = face_recognition.compare_faces([face_enc], known_encodings[item])
-        print("face_recognition.compare_faces=", result)
+        # print("face_recognition.compare_faces=", result)
         
         if result[0]:
           known_encodings.append(face_enc)
-          print("Same person!")
+          # print("Same person!")
           break
         else:
-          print("Another person!")
+          # print("Another person!")
           break
   
-  print(known_encodings)
-  print(f"Length {len(known_encodings)}")
-
+  # print(known_encodings)
+  # print(f"Length {len(known_encodings)}")
+  
+  data = {
+    "name": name,
+    "encodings": known_encodings
+  }
+  
+  with open(f"{name}_encodings.pickle", "wb") as file:
+    file.write(pickle.dumps(data))
+  
+  return f"[INFO] File {name}_encodings.pickle successfully created"
 
 
 
